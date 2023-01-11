39d595d625f2f92895da21f516672259c57857be
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-01-11 20:53:52 +0100

    update 4 task all done

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,14 @@
 const decrem = document.querySelector("#counter button[data-action = decrement]")
 const increm = document.querySelector("#counter button[data-action = increment]")
+const span = document.querySelector("#value")
 
 let counterValue = 0
 
 const dec = () => {
-    console.log(counterValue -=1)
+    console.log( span.textContent = (counterValue -=1))
 }
 const inc = () => {
-    console.log(counterValue +=1)
+    console.log(span.textContent = (counterValue +=1))
 }
 decrem.addEventListener("click", dec)
 increm.addEventListener("click",inc)
--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const events = document.querySelector("input#name-input")
 const text = document.querySelector("span#name-output")
 
 const replases = (elem) => {
-    console.log(1)
     return text.textContent = elem.currentTarget.value || "Anonymous"
 }
 
