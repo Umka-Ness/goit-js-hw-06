bb259d056c8d123bc85ee9ab28fe17c83c66b56b
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-01-12 23:13:42 +0100

    task 7 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,10 @@
 const input = document.querySelector("input#font-size-control")
 const text = document.querySelector("span#text")
 
+text.style.fontSize = `${input.value}px`
+
 const evente =() => {
      text.style.fontSize = `${input.value}px`
 }
-
 
 input.addEventListener('input',evente)
