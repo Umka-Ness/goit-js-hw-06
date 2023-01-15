625f9e9c1a04513aa2d41f4d268a1089fd09179a
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-01-15 22:21:18 +0100

    update all

--- js/task-09.js
+++ js/task-09.js
@@ -2,7 +2,7 @@ const btn = document.querySelector(".change-color")
 const spanColor = document.querySelector(".color")
 
 const  getRandomHexColor = () => { 
-  console.log(1)
+
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
